9793c3e2d1b832c9664b254566dfcf4a7ed2d99b
Devanshu Modi <devmodi154@gmail.com>
2020-08-25 17:03:40 +0530

    Added New Look, Player v Computer Functionality

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 var restart=document.querySelector("#b")
 
-var player1= prompt("Player One:Enter your Name,you will be blue");
+var player1= "Blue";
 var player1Color='rgb(86,151,255)';
 var player1_wins=0
 
-var player2= prompt("Player Two:Enter your Name,you will be red");
+var player2= "Red";
 var player2Color='rgb(237,45,73)';
 var player2_wins=0
 
 var game_on= true;
 var table = $('table tr');
-var circles= document.querySelectorAll('.board button')
+var circles= document.querySelectorAll('.board button');
+var scoreboard = document.getElementById('Scoreboard');
+var restartButton = document.getElementById('b');
+var blue = document.getElementById('blue');
+var red = document.getElementById('red');
+red.style.backgroundColor = 'rgb(128,128,128)';
+
 
 function reportWin(rowNum,colNum)
 {
@@ -136,7 +142,6 @@ function drawCheck()
 }
 
 
-var currentPlayer = 1;
 var currentName = player1;
 var currentColor = player1Color;
 
@@ -146,18 +151,10 @@ $('h3').text(player1+" it is your turn,pick a column to drop in!");
 function clearBoard(){
 	
 	$('.board button').css('background-color','rgb(128, 128, 128)');
-	$('#b').css('visibility','hidden');
-	// player1= prompt("Player One:Enter your Name,you will be blue");
-	// player1Color='rgb(86,151,255)';
-
-	// player2= prompt("Player Two:Enter your Name,you will be red");
-	// player2Color='rgb(237,45,73)';
-	$('h1').text(player1+":"+player1_wins+" | "+player2+":"+player2_wins);
+	restartButton.style.visibility = 'hidden';
+	scoreboard.innerHTML = player1+"  "+player1_wins+" <br> "+player2+"  "+player2_wins;
 	$('h2').text('Connect 4 chips to win!');
-	$('h3').text("Let's Start");
-	console.log("Q");
 	game_on=true;
-	currentPlayer = 1;
 	currentName = player1;
 	currentColor = player1Color;
 
@@ -165,56 +162,56 @@ function clearBoard(){
 
 restart.addEventListener('click',clearBoard);
 
+function resultCheck(col) {
+	if(currentColor === 'rgb(86,151,255)') {
+		blue.style.backgroundColor = 'rgb(128,128,128)';
+		red.style.backgroundColor = 'rgb(237,45,73)';
+	} else {
+		red.style.backgroundColor = 'rgb(128,128,128)';
+		blue.style.backgroundColor = 'rgb(86,151,255)';
+	}
+
+	var bottomAvail = checkBottom(col);
+	changeColor(bottomAvail,col,currentColor);
+	if(horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck())
+	{
+		scoreboard.innerHTML = currentName+" You have won!"; 
+		$('h2').text('');
+		$('h3').text('');
+		restartButton.style.visibility = 'visible';
+
+		if(player1===currentName)
+			player1_wins++;
+		else
+			player2_wins++;
+		
+		game_on=false;
+	} else if(drawCheck()) {
+			$('h1').text("Game Draw!");
+			$('h2').text('');
+			$('h3').text('');
+			restartButton.style.visibility = 'visible';
+			game_on=false;
+	}
+}
+
 
 $('.board button').on('click',function(){
 
 	if(game_on==true)
 	{
 		var col = $(this).closest('td').index();
-		var bottomAvail=checkBottom(col);
-		changeColor(bottomAvail,col,currentColor);
-		if(horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck())
+		resultCheck(col, currentColor);
+		if( game_on )
 		{
-			$('h1').text(currentName+" You have won!");
-			$('h2').text('');
-			$('h3').text('');
-			$('#b').css('visibility','visible');
-
-			if(player1===currentName)
-			{
-				player1_wins++;
-			}
-			else
-			{
-				player2_wins++;
-			}
-			game_on=false;
-		}
-		else
-			if(drawCheck())
-			{
-				$('h1').text("Game Draw!");
-				$('h2').text('');
-				$('h3').text('');
-				$('#b').css('visibility','visible');
-				game_on=false;
-			}
-
-		currentPlayer = currentPlayer*-1;
-
-		if(currentPlayer === 1 && game_on)
-		{
-			currentName = player1;
-			$('h3').text(currentName+" it is your turn.");
+			currentName = player2;
+			currentColor = player2Color;
+			var choice = [col-1,col,col+1];
+			col = choice[Math.floor(Math.random() * 3)]%7;
+			resultCheck(col, currentColor);
 			currentColor = player1Color;
+			currentName = player1;
 		}
-		else
-			if(game_on)
-			{
-				currentName = player2;
-				$('h3').text(currentName+" it is your turn.");	
-				currentColor = player2Color;
-			}
 
 	}
 })
